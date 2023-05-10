4eef0cc0044e7a9a5995b819b7ecd1bc792ab341
Sam Bao <sam.b5687@gmail.com>
2023-05-10 13:15:21 -0600

    adding back privacy policy page, also making playlist id sample more clear

---
+++ Models/PrivacyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Youtube2Spotify.Models
+{
+    public class PrivacyModel
+    {
+    }
+}
