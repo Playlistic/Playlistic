09cb95b1f17a0a07790e67181d51b7c1941c58d2
Sam Bao <sam.b5687@gmail.com>
2023-05-10 15:15:53 -0600

    working privacy policy page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Youtube2Spotify.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-       
+        private HomeModel homeModel;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -17,12 +17,12 @@ namespace Youtube2Spotify.Controllers
 
         public IActionResult Index()
         {
-            HomeModel homeModel = new HomeModel();
+            homeModel = new HomeModel();
             homeModel.Authenticated = false;
             HttpContext.Session.TryGetValue("access_token", out byte[] access_token);
             HttpContext.Session.TryGetValue("expire_time", out byte[] expire_time_raw);
 
-           
+
             if (access_token != null && expire_time_raw != null)
             {
                 DateTime expire_time = DateTime.Parse(new string(System.Text.Encoding.Default.GetString(expire_time_raw)));
@@ -34,11 +34,6 @@ namespace Youtube2Spotify.Controllers
             }
 
             return View(homeModel);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Controllers/PrivacyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Youtube2Spotify.Models;
+
+namespace Youtube2Spotify.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
